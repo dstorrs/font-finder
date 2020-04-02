fa316eef64ee8525ad741479f132246b4a0acf85
dstorrs <dstorrs.public@gmail.com>
2020-04-01 23:08:32 -0400

    Populated info.rkt

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/font-finder.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "Minimal utilities for locating font files on your computer")
+(define version "0.1")
 (define pkg-authors '(dstorrs))
