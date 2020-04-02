cf50fdf3c3c3eb935c488fae8a334ee83161d9ff
dstorrs <dstorrs.public@gmail.com>
2020-04-01 22:57:30 -0400

    Wrote the code, wrote the README.md

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-This is a Racket library for locating font files on your computer.
+# Purpose
 
+This Racket library searches standard locations on your computer for TrueType, OpenType, and Postscript Type 1 (PT1) fonts.
+
+**This library is very early-stage and incomplete**
+
+# Font Directories 
+
+The following are the standard locations for fonts based on your OS:
+
+    Windows:  C:\Windows\Fonts  (TT and OT)
+              C:\psfonts\pfm    (PT1)
+
+
+    macOS:    /System/Library/Fonts
+              /Library/Fonts
+              /Users/{name}/Library/Fonts
+
+
+    unix:     Throws an exception.  Font locations are not standardized.  A future version will handle this better.
+
+# Font Extensions
+
+Fonts are identified based on their extension.  Expected extensions are:
+
+  TrueType: .ttf, .ttc
+  OpenType: .ttf, .otf
+  PT1     : .pfm, .pfb
+
+See the Warnings section below.
+
+# Interface
+
+ `(known-fonts)`             : Returns a list of all font files it can find
+ `(populate-known-fonts)`    : Re-scans the disk to refresh the value returned by `(known-fonts)`
+ `(system-font-folders)`     : Returns the list of system folders that will be searched for fonts.
+ `(macos-user-font-folders)` : Returns the list of /Users/{name}/Library/Fonts directories that exist on this Mac.
+
+Note:  You don't need to explicitly call `populate-known-fonts`, since `known-fonts` will take care of that if it has not been done yet.  After `populate-known-fonts` is called, the list will be cached and future calls to `known-fonts` will return the cached copy without re-checking the disk.  You may call `populate-known-fonts` in order to refresh the list (e.g. if you install more fonts).
+
+# **WARNINGS**
+
+* PT1 fonts are problematic in that they consist of two files; this module does not worry about that, it simply returns both as separate items and it's your job to match them up.  They will often not be contiguous in the list.
+* The returned list is not guaranteed to be in any particular order.
+* font-finder will not notice fonts that aren't in one of the expected locations
+* Fonts are identified based on their extension.  If it doesn't have one of the expected extensions, it will not be recognized and therefore it will not be in the list of known fonts.
+* The list returned by `known-fonts` only updates when `populate-known-fonts` is called.  This is done automatically the first time you call `known-fonts`, but if you install or delete fonts then your list will be stale and you'll need to call `populate-known-fonts` again in order to refresh it.
--- main.rkt
+++ main.rkt
@@ -1,50 +1,62 @@
 #lang racket/base
 
-(module+ test
-  (require rackunit))
+(require racket/require
+         (multi-in racket (bool contract/base contract/region file list match path splicing)))
 
-;; Notice
-;; To install (from within the package directory):
-;;   $ raco pkg install
-;; To install (once uploaded to pkgs.racket-lang.org):
-;;   $ raco pkg install <<name>>
-;; To uninstall:
-;;   $ raco pkg remove <<name>>
-;; To view documentation:
-;;   $ raco docs <<name>>
-;;
-;; For your convenience, we have included LICENSE-MIT and LICENSE-APACHE files.
-;; If you would prefer to use a different license, replace those files with the
-;; desired license.
-;;
-;; Some users like to add a `private/` directory, place auxiliary files there,
-;; and require them in `main.rkt`.
-;;
-;; See the current version of the racket style guide here:
-;; http://docs.racket-lang.org/style/index.html
+(provide macos-user-font-folders
+         system-font-folders
+         populate-known-fonts
+         known-fonts)
 
-;; Code here
+;;----------------------------------------------------------------------
+
+; Return the list of existing /Users/{name}/Library/Fonts directories
+(define/contract (macos-user-font-folders)
+  (-> (listof path?))
+
+  (define Users-dir  (build-path "/" "Users"))
+  (define suffix (build-path "Library" "Fonts"))
+  (filter directory-exists?
+          (for/list ([username (in-list (directory-list Users-dir))])
+            (build-path Users-dir username suffix))))
+
+;;----------------------------------------------------------------------
+
+; Return the list of standard font folders for this operating system
+(splicing-let ([sys-font-folders
+                (match (system-type)
+                  ['macosx  (list* (build-path "/" "System" "Library" "Fonts")
+                                   (build-path "/" "Library" "Fonts")
+                                   (macos-user-font-folders))]
+                  ['unix    (error "Unix and Linux do not have a standard location for fonts")] ; @@TODO Handle this more gracefully
+                  ['windows (list (build-path "C:\\" "Windows" "Fonts")
+                                  (build-path "C:\\" "psfonts" "pfm"))])])
+
+  (define/contract (system-font-folders)
+    (-> (listof path?))
+    sys-font-folders))
+
+;;----------------------------------------------------------------------
+
+; Scan the disk to locate font files
+(define kf #f)
+(define (populate-known-fonts)
+  (set! kf (apply append
+                  (filter-not empty?
+                              (for/list ([folder (system-font-folders)])
+                                (find-files (λ (p)
+                                              ((or/c #".ttf" #".ttc" #".otf" #".pfm" #".pfb")
+                                               (path-get-extension p)))
+                                            folder))))))
+
+;;----------------------------------------------------------------------
+
+; Return the list of known font files.  Will call populate-known-fonts
+; if it hasn't been called
+(define/contract (known-fonts)
+  (-> (listof path?))
+  (when (false? kf)
+    (populate-known-fonts))
+  kf)
 
 
-
-(module+ test
-  ;; Any code in this `test` submodule runs when this file is run using DrRacket
-  ;; or with `raco test`. The code here does not run when this file is
-  ;; required by another module.
-
-  (check-equal? (+ 2 2) 4))
-
-(module+ main
-  ;; (Optional) main submodule. Put code here if you need it to be executed when
-  ;; this file is run using DrRacket or the `racket` executable.  The code here
-  ;; does not run when this file is required by another module. Documentation:
-  ;; http://docs.racket-lang.org/guide/Module_Syntax.html#%28part._main-and-test%29
-
-  (require racket/cmdline)
-  (define who (box "world"))
-  (command-line
-    #:program "my-program"
-    #:once-each
-    [("-n" "--name") name "Who to say hello to" (set-box! who name)]
-    #:args ()
-    (printf "hello ~a~n" (unbox who))))
